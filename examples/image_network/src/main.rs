b1c9e65f3d745a9b06fac84b5f995ba2815b649b
Adia Robbie <adyaro37@gmail.com>
2021-11-28 18:42:48 +0700

    Update main.rs

--- examples/image_network/src/main.rs
+++ examples/image_network/src/main.rs
@@ -1,22 +1,22 @@
 mod framework;
-use asynchron::{Futurize, Futurized, InnerTaskHandle, Progress};
 use backend::epi;
 use egui_fltk_frontend as frontend;
 use egui_wgpu_backend as backend;
+use flowync::Leaper;
 use frontend::{egui, fltk, ImgWidget, ImgWidgetExt};
-use std::{borrow::Cow, time::Duration};
+use std::{thread, time::Duration};
 const INTEGRATION_NAME: &str = "egui + fltk + wgpu-backend";
 
-struct ImageDemoReqwest<'a> {
+struct ImageDemoReqwest {
     image_widget: Option<ImgWidget>,
     tokio_rt: tokio::runtime::Runtime,
-    task: Option<Futurized<(), Vec<u8>>>,
-    fetch_btn_label: Cow<'a, str>,
-    err_label: Option<Cow<'a, str>>,
+    task: Option<Leaper<Vec<u8>>>,
+    fetch_btn_label: String,
+    err_label: Option<String>,
     seed: usize,
 }
 
-impl<'a> Default for ImageDemoReqwest<'a> {
+impl Default for ImageDemoReqwest {
     fn default() -> Self {
         let tokio_rt = tokio::runtime::Builder::new_multi_thread()
             .enable_all()
@@ -33,7 +33,23 @@ impl<'a> Default for ImageDemoReqwest<'a> {
     }
 }
 
-impl<'a> epi::App for ImageDemoReqwest<'a> {
+async fn fecth(url: &str, timeout: Duration) -> Result<Vec<u8>, reqwest::Error> {
+    let client = reqwest::Client::builder()
+        .connect_timeout(timeout)
+        .build()?;
+    let request = client.get(url).build()?;
+    let response = client.execute(request).await?;
+
+    println!("Status: {}\n", response.status());
+    for value in response.headers().values() {
+        println!("{}\n", value.to_str().unwrap());
+    }
+
+    let content = response.bytes().await?.to_vec();
+    Ok(content)
+}
+
+impl epi::App for ImageDemoReqwest {
     fn name(&self) -> &str {
         "world"
     }
@@ -55,48 +71,23 @@ impl<'a> epi::App for ImageDemoReqwest<'a> {
                 if task.is_none() {
                     let current_seed = *seed;
                     let rt = tokio_rt.handle().clone();
-                    let new_task = Futurize::task(
-                        current_seed,
-                        move |_task: InnerTaskHandle| -> Progress<(), Vec<u8>> {
+                    let new_task = Leaper::new(current_seed);
+                    thread::spawn({
+                        let handle = new_task.handle();
+                        move || {
                             rt.block_on(async {
-                                let timeout = Duration::from_secs(3);
-                                let client = match reqwest::Client::builder()
-                                    .connect_timeout(timeout)
-                                    .build()
-                                {
-                                    Ok(client) => client,
-                                    Err(e) => return Progress::Error(e.to_string().into()),
-                                };
-
                                 // Url to .jpg image
                                 let url =
                                     format!("https://picsum.photos/seed/{}/640/480", current_seed);
-
-                                let request = match client.get(url).build() {
-                                    Ok(request) => request,
-                                    Err(e) => return Progress::Error(e.to_string().into()),
+                                let timeout = Duration::from_secs(3);
+                                let content = match fecth(&url, timeout).await {
+                                    Ok(content) => content,
+                                    Err(e) => return handle.err(e.to_string()),
                                 };
-
-                                let response = match client.execute(request).await {
-                                    Ok(response) => response,
-                                    Err(e) => return Progress::Error(e.to_string().into()),
-                                };
-
-                                println!("Status: {}\n", response.status());
-
-                                for value in response.headers().values() {
-                                    println!("{}\n", value.to_str().unwrap());
-                                }
-
-                                let content = match response.bytes().await {
-                                    Ok(r) => r,
-                                    Err(e) => return Progress::Error(e.to_string().into()),
-                                };
-                                Progress::Completed(content.to_vec())
-                            })
-                        },
-                    );
-                    new_task.try_do();
+                                handle.ok(content);
+                            });
+                        }
+                    });
                     *task = Some(new_task);
                     *seed += 1;
                 } else {
@@ -108,11 +99,11 @@ impl<'a> epi::App for ImageDemoReqwest<'a> {
 
             // Only resolve if task is some.
             if let Some(this) = task {
+                *fetch_btn_label = "fetching...".into();
                 let mut task_should_free = false;
-                this.try_resolve(|progress, done| {
-                    match progress {
-                        Progress::Completed(jpg) => {
-                            // Just to make sure, free unused texture id.
+                this.try_catch(|result| {
+                    match result {
+                        Ok(jpg) => {
                             if let Some(this) = image_widget {
                                 frame.tex_allocator().free(this.texture_id());
                             }
@@ -121,26 +112,24 @@ impl<'a> epi::App for ImageDemoReqwest<'a> {
                                 .into_img_widget(frame);
                             *err_label = None;
                         }
-                        Progress::Error(e) => {
-                            *err_label = Some(e.to_string().into());
+                        Err(e) => {
+                            *err_label = Some(e);
                             // And here.
                             if let Some(this) = image_widget {
                                 frame.tex_allocator().free(this.texture_id());
                             }
                             *image_widget = None;
                         }
-                        Progress::Current(_) => *fetch_btn_label = "fetching...".into(),
-                        _ => (),
                     }
-                    if done {
-                        // Redraw
-                        frame.repaint_signal();
-                        task_should_free = true;
-                        *fetch_btn_label = "fetch next image".into();
-                    }
+
+                    // Redraw
+                    frame.repaint_signal();
+                    *fetch_btn_label = "fetch next image".into();
+
+                    // Free task.
+                    task_should_free = true;
                 });
 
-                // Free task.
                 if task_should_free {
                     *task = None;
                 }
